59f9ebf70bcc5cc50536201314206b19a06f06b5
masnagam <masnagam@gmail.com>
2020-07-06 00:24:31 +0900

    test: improve cli_tests.sh

--- test/cli_tests.sh
+++ test/cli_tests.sh
@@ -1,89 +1,60 @@
 #!/bin/sh
 
+set -u
+
 MIRAKC_ARIB="$1"
 
 export MIRAKC_ARIB_LOG=none
 
-# show help
+assert() {
+  expected=$1
+  cmd="$2"
+  sh -c "$cmd </dev/null >/dev/null"
+  actual=$?
+  if [ $expected -eq $actual ]; then
+    echo "PASS: $cmd"
+  else
+    echo "FAIL: $cmd: expectd($expected) actual($actual)"
+    exit 1
+  fi
+}
 
-echo 'testing help...'
 for opt in '-h' '--help'
 do
-  $MIRAKC_ARIB $opt >/dev/null
+  assert 0 "$MIRAKC_ARIB $opt"
   for cmd in 'scan-services' 'sync-clocks' 'collect-eits' 'collect-logos' \
              'filter-service' 'filter-program' 'track-airtime' 'seek-start' \
              'print-timetable'
   do
-    $MIRAKC_ARIB $cmd $opt >/dev/null
+    assert 0 "$MIRAKC_ARIB $cmd $opt"
   done
 done
-echo 'PASS'
 
-# show version
+assert 0 "$MIRAKC_ARIB --version"
 
-echo 'testing version...'
-$MIRAKC_ARIB --version >/dev/null
-echo 'PASS'
+assert 1 "$MIRAKC_ARIB scan-services"
+assert 1 "$MIRAKC_ARIB scan-services --sids=1 --sids=0xFFFF --xsids=1 --xsids=0xFFFF"
 
-# scan-services
+assert 1 "$MIRAKC_ARIB sync-clocks"
+assert 1 "$MIRAKC_ARIB sync-clocks --sids=1 --sids=0xFFFF --xsids=1 --xsids=0xFFFF"
 
-echo 'testing scan-services...'
-$MIRAKC_ARIB scan-services </dev/null >/dev/null
-$MIRAKC_ARIB scan-services \
-  --sids=1 --sids=0xFFFF --xsids=1 --xsids=0xFFFF </dev/null >/dev/null
-echo 'PASS'
+assert 1 "$MIRAKC_ARIB collect-eits"
+assert 1 "$MIRAKC_ARIB collect-eits --sids=1 --sids=0xFFFF --xsids=1 --xsids=0xFFFF --time-limit=0x7FFFFFFFFFFFFFFF --streaming"
+assert 134 "$MIRAKC_ARIB collect-eits --time-limit=0xFFFFFFFFFFFFFFFF"
 
-# sync-clocks
+assert 0 "$MIRAKC_ARIB filter-service --sid=1"
+assert 0 "$MIRAKC_ARIB filter-service --sid=0xFFFF"
 
-echo 'testing sync-clocks...'
-$MIRAKC_ARIB sync-clocks </dev/null >/dev/null
-$MIRAKC_ARIB sync-clocks \
-  --sids=1 --sids=0xFFFF --xsids=1 --xsids=0xFFFF </dev/null >/dev/null
-echo 'PASS'
+assert 0 "$MIRAKC_ARIB filter-program --sid=1 --eid=1 --clock-pcr=1 --clock-time=1"
+assert 0 "$MIRAKC_ARIB filter-program --sid=0xFFFF --eid=0xFFFF --clock-pcr=0xFFFFFFFFFFFFFFFF --clock-time=-9223372036854775808 --start-margin=1 --end-margin=1 --pre-streaming"
+assert 134 "$MIRAKC_ARIB filter-program --sid=1 --eid=1 --clock-pcr=0xFFFFFFFFFFFFFFFFF --clock-time=1"
+assert 134 "$MIRAKC_ARIB filter-program --sid=1 --eid=1 --clock-pcr=1 --clock-time=-9223372036854775809"
 
-# collect-eits
+assert 0 "$MIRAKC_ARIB track-airtime --sid=1 --eid=1"
+assert 0 "$MIRAKC_ARIB track-airtime --sid=0xFFFF --eid=0xFFFF"
 
-echo 'testing collect-eits...'
-$MIRAKC_ARIB collect-eits </dev/null >/dev/null
-$MIRAKC_ARIB collect-eits \
-  --sids=1 --sids=0xFFFF --xsids=1 --xsids=0xFFFF \
-  --time-limit=0x7FFFFFFFFFFFFFFF --streaming </dev/null >/dev/null
-echo 'PASS'
+assert 0 "$MIRAKC_ARIB seek-start --sid=1 --max-duration=1"
+assert 0 "$MIRAKC_ARIB seek-start --sid=0xFFFF --max-duration=0x7FFFFFFFFFFFFFFF --max-packets=0x7FFFFFFF"
+assert 134 "$MIRAKC_ARIB seek-start --sid=0xFFFF --max-duration=0xFFFFFFFFFFFFFFFF --max-packets=0x7FFFFFFF"
 
-# filter-service
-
-echo 'testing filter-service...'
-$MIRAKC_ARIB filter-service --sid=1 </dev/null >/dev/null
-$MIRAKC_ARIB filter-service --sid=0xFFFF </dev/null >/dev/null
-echo 'PASS'
-
-# filter-program
-
-echo 'testing filter-program...'
-$MIRAKC_ARIB filter-program --sid=1 --eid=1 --clock-pcr=1 --clock-time=1 \
-  </dev/null >/dev/null
-$MIRAKC_ARIB filter-program --sid=0xFFFF --eid=0xFFFF \
-  --clock-pcr=0x7FFFFFFFFFFFFFFF --clock-time=-9223372036854775808 \
-  --start-margin=1 --end-margin=1 --pre-streaming </dev/null >/dev/null
-echo 'PASS'
-
-# track-airtime
-
-echo 'testing track-airtime...'
-$MIRAKC_ARIB track-airtime --sid=1 --eid=1 </dev/null >/dev/null
-$MIRAKC_ARIB track-airtime --sid=0xFFFF --eid=0xFFFF </dev/null >/dev/null
-echo 'PASS'
-
-# seek-start
-
-echo 'testing seek-start...'
-$MIRAKC_ARIB seek-start --sid=1 </dev/null >/dev/null
-$MIRAKC_ARIB seek-start --sid=0xFFFF --max-duration=0x7FFFFFFFFFFFFFFF \
-  --max-packets=0x7FFFFFFF </dev/null >/dev/null
-echo 'PASS'
-
-# print-timetable
-
-echo 'testing print-timetable...'
-$MIRAKC_ARIB print-timetable </dev/null >/dev/null
-echo 'PASS'
+assert 0 "$MIRAKC_ARIB print-timetable"
