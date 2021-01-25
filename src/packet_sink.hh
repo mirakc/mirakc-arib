795830f90663c008120d6ac0bc447b74663707c2
masnagam <masnagam@gmail.com>
2021-01-25 17:47:19 +0900

    perf: improve the performance of filter-* and seek-start
    Introduce the write buffer.
    
    This change may improve the performance of a program which reads data from mirakc-arib.  In fact,
    this change improves the CPU usage of `nc` in the following command on macOS:
    
      curl -s http://mirakc:40772/api/channels/GR/27/stream | \
        mirakc-arib filter-service --sid=1024 | nc tsd 40773 >/dev/null

--- src/packet_sink.hh
+++ src/packet_sink.hh
@@ -27,23 +27,60 @@ class StdoutSink final : public PacketSink {
   StdoutSink() = default;
   ~StdoutSink() override {}
 
+  bool End() override {
+    return Flush();
+  }
+
   bool HandlePacket(const ts::TSPacket& packet) override {
-    size_t nwritten = 0;
-    while (nwritten < ts::PKT_SIZE) {
-      ssize_t res =
-          write(kStdoutFd, packet.b + nwritten, ts::PKT_SIZE - nwritten);
-      if (res < 0) {
-        MIRAKC_ARIB_ERROR(
-            "Failed to write packet: {} ({})", std::strerror(errno), errno);
-        return false;
-      }
-      nwritten += res;
+    if (pos_ + ts::PKT_SIZE < kBufferSize) {
+      std::memcpy(buf_ + pos_, packet.b, ts::PKT_SIZE);
+      pos_ += ts::PKT_SIZE;
+      return true;
     }
+
+    auto remaining = kBufferSize - pos_;
+    std::memcpy(buf_ + pos_, packet.b, remaining);
+    pos_ += remaining;
+    MIRAKC_ARIB_ASSERT(pos_ == kBufferSize);
+    if (!Flush()) {
+      return false;
+    }
+
+    if (ts::PKT_SIZE > remaining) {
+      MIRAKC_ARIB_ASSERT(pos_ == 0);
+      std::memcpy(buf_, packet.b + remaining, ts::PKT_SIZE - remaining);
+      pos_ = ts::PKT_SIZE - remaining;
+    }
+
     return true;
   }
 
  private:
   static constexpr int kStdoutFd = 1;
+
+  // 4 pages for the write buffer.
+  // 16 pages for pipe in Linux by default.
+  // See https://man7.org/linux/man-pages/man7/pipe.7.html
+  static constexpr size_t kBufferSize = 4096 * 4;
+
+  bool Flush() {
+    size_t nwritten = 0;
+    while (nwritten < pos_) {
+      auto res = write(kStdoutFd, buf_ + nwritten, pos_ - nwritten);
+      if (res < 0) {
+        MIRAKC_ARIB_ERROR(
+            "Failed to write packets: {} ({})", std::strerror(errno), errno);
+        return false;
+      }
+      nwritten += res;
+    }
+    MIRAKC_ARIB_ASSERT(nwritten == pos_);
+    pos_ = 0;
+    return true;
+  }
+
+  uint8_t buf_[kBufferSize];
+  size_t pos_ = 0;
 
   MIRAKC_ARIB_NON_COPYABLE(StdoutSink);
 };
