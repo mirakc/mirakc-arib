d68596a95d48a4b9befc9cf9ec694afa33c95679
masnagam <masnagam@gmail.com>
2022-12-16 09:21:07 +0900

    fix: change the exit code for retry

--- src/exit_code.hh
+++ src/exit_code.hh
@@ -7,4 +7,7 @@ static_assert(EXIT_FAILURE == 1);
 
 // This exit code is returned when `filter-program` stopped before the program
 // starts.  This situation occurs when the program was canceled or rescheduled.
-#define EXIT_RETRY 2
+//
+// There are many 'reserved' code as described in:
+// https://tldp.org/LDP/abs/html/exitcodes.html
+#define EXIT_RETRY 222
